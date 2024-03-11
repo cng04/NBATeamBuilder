afcfbde8cd34a0b634a3243bd2d1da80a571ed0a
cng04 <calebng.ca@gmail.com>
2024-03-11 17:28:58 -0400

    Initial React Project commit

---
+++ team-builder-ui/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Court from './components/Court';
+
+function App() {
+  return (
+    <Court>Hello World</Court>
+  );
+}
+
+export default App;
---
+++ team-builder-ui/src/components/Court.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Court(props) {
+  return (
+    <>
+    <div>Court</div>
+    <div>{props.children}</div>
+    </>
+  )
+}
